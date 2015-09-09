500b7295f4e6afc835af2486e98af58cfa1197a8
marieke-bijlsma <mariekebijlsma01@gmail.com>
2015-09-09 12:53:54 +0200

    Started repository for graduation project and added initial Java tools

---
+++ src/main/java/org/molgenis/graduation/config/Config.java
@@ -0,0 +1,11 @@
+package org.molgenis.graduation.config;
+
+import org.molgenis.CommandLineOnlyConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@CommandLineOnlyConfiguration
+public class Config
+{
+
+}
