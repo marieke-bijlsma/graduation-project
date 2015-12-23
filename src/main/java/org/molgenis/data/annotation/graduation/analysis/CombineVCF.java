91683693cbf8e64b9a26ca200404b87b96691d6e
marieke-bijlsma <mariekebijlsma01@gmail.com>
2015-12-23 17:48:51 +0100

    Expanded AnnotatorUtil class with new annotators. Made new methods to chain annotators. Refactored CombineVcf class.

--- src/main/java/org/molgenis/data/annotation/graduation/analysis/CombineVCF.java
+++ src/main/java/org/molgenis/data/annotation/graduation/analysis/CombineVCF.java
@@ -1,17 +1,18 @@
 package org.molgenis.data.annotation.graduation.analysis;
 
+import static org.molgenis.data.annotation.graduation.utils.AnnotatorUtils.annotateWithCadd;
+import static org.molgenis.data.annotation.graduation.utils.AnnotatorUtils.annotateWithExac;
+import static org.molgenis.data.annotation.graduation.utils.AnnotatorUtils.annotateWithGoNl;
+import static org.molgenis.data.annotation.graduation.utils.AnnotatorUtils.annotateWithThousandGenomes;
+import static org.molgenis.data.vcf.utils.VcfUtils.convertToVCF;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.Iterator;
-import java.util.Map;
 
 import org.molgenis.data.Entity;
-import org.molgenis.data.annotation.RepositoryAnnotator;
-import org.molgenis.data.annotation.cmd.CommandLineAnnotatorConfig;
+import org.molgenis.data.annotation.graduation.utils.AnnotatorUtils;
 import org.molgenis.data.vcf.VcfRepository;
-import org.molgenis.data.vcf.utils.VcfUtils;
-import org.molgenis.util.ApplicationContextProvider;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -24,10 +25,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class CombineVCF
 {
-	File vcfFile;
-	File dataFile;
-	File outputFile;
-	
+	private File vcfFile;
+	private File outputFile;
+
 	/**
 	 * Reads a VCF file and the associated data file, parses it, and writes it to a new file.
 	 * 
@@ -40,47 +40,36 @@ public class CombineVCF
 	 * @throws Exception
 	 *             when output file is not correct
 	 */
-	public void go() throws Exception
+	public void annotateVcfFile() throws Exception
 	{
 		@SuppressWarnings("resource")
 		PrintWriter pw = new PrintWriter(outputFile, "UTF-8");
 
-		@SuppressWarnings("resource")
-		Iterator<Entity> vcf = new VcfRepository(vcfFile, "vcf").iterator();
+		VcfRepository vcfRepository = new VcfRepository(vcfFile, "vcf");
 
-		ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
-		Map<String, RepositoryAnnotator> annotators = applicationContext.getBeansOfType(RepositoryAnnotator.class);
-		RepositoryAnnotator exacAnnotator = annotators.get("exac");
-		// RepositoryAnnotator caddAnnotator = annotators.get("cadd");
-		// RepositoryAnnotator gonlAnnotator = annotators.get("gonl");
-		// RepositoryAnnotator thousandGenomesAnnotator = annotators.get("thousandGenomes");
-		exacAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(dataFile.getAbsolutePath());
-		// caddAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(dataFile.getAbsolutePath());
-		// gonlAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(dataFile.getAbsolutePath());
-		// thousandGenomesAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(dataFile.getAbsolutePath());
-		Iterator<Entity> vcfWithExac = exacAnnotator.annotate(vcf);
-		// Iterator<Entity> vcfWithCadd = caddAnnotator.annotate(vcf);
-		// Iterator<Entity> vcfWithGonl = gonlAnnotator.annotate(vcf);
-		// Iterator<Entity> vcfWithThousandGenomes = thousandGenomesAnnotator.annotate(vcf);
+		Iterator<Entity> vcfRepositoryWithExac = annotateWithExac(vcfRepository);
+		Iterator<Entity> vcfRepositoryWithExacAndCadd = annotateWithCadd(vcfRepositoryWithExac);
+		Iterator<Entity> vcfRepositoryWithExacAndCaddAndGoNl = annotateWithGoNl(vcfRepositoryWithExacAndCadd);
+		Iterator<Entity> vcfRepositoryWithExacAndCaddAndGoNlAndThousandGenomes = annotateWithThousandGenomes(vcfRepositoryWithExacAndCaddAndGoNl);
 
-		while (vcfWithExac.hasNext())
+		while (vcfRepositoryWithExacAndCaddAndGoNlAndThousandGenomes.hasNext())
 		{
-			Entity record = vcfWithExac.next();
+			Entity record = vcfRepositoryWithExac.next();
 
 			String exac_af_STR = record.get("EXAC_AF") == null ? null : record.get("EXAC_AF").toString();
 			String exac_ac_hom_STR = record.get("EXAC_AC_HOM") == null ? "" : record.get("EXAC_AC_HOM").toString();
 			String exac_ac_het_STR = record.get("EXAC_AC_HET") == null ? "" : record.get("EXAC_AC_HET").toString();
-			// String gonl_af_STR = record.get("GoNL_AF") == null ? null : record.get("GoNL_AF").toString();
-			// String thousandG_af_STR = record.get("Thousand_Genomes_AF") == null ? null : record.get(
-			// "Thousand_Genomes_AF").toString();
-			// String cadd_score = record.get("CADD_SCALED") == null ? null : record.get("CADD_SCALED").toString();
+			String gonl_af_STR = record.get("GoNL_AF") == null ? null : record.get("GoNL_AF").toString();
+			String thousandG_af_STR = record.get("Thousand_Genomes_AF") == null ? null : record.get(
+					"Thousand_Genomes_AF").toString();
+			String cadd_score = record.get("CADD_SCALED") == null ? null : record.get("CADD_SCALED").toString();
 
-			String vcfEntry = VcfUtils.convertToVCF(record, false);
+			String vcfEntry = convertToVCF(record, false);
 
 			pw.println(vcfEntry + "\t" + exac_af_STR + "\t" + exac_ac_hom_STR + "\t" + exac_ac_het_STR);
-			// pw.println(vcfEntry + "\t" + gonl_af_STR
-			// pw.println(vcfEntry + "\t" + thousandG_af_STR
-			// pw.println(vcfEntry + "\t" + cadd_score
+			pw.println(vcfEntry + "\t" + gonl_af_STR);
+			pw.println(vcfEntry + "\t" + thousandG_af_STR);
+			pw.println(vcfEntry + "\t" + cadd_score);
 		}
 	}
 
@@ -94,13 +83,13 @@ public class CombineVCF
 	 */
 	public static void main(String[] args) throws Exception
 	{
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("org.molgenis.data.annotation");
-		ctx.register(CommandLineAnnotatorConfig.class);
-		CombineVCF main = ctx.getBean(CombineVCF.class);
-		main.parseCommandLineArgs(args);
-		CombineVCF combineVCF = new CombineVCF();
-		combineVCF.go();
-		ctx.close();
+		AnnotationConfigApplicationContext context = AnnotatorUtils.registerCommandLineAnnotator();
+		CombineVCF combineVCF = context.getBean(CombineVCF.class);
+
+		combineVCF.parseCommandLineArgs(args);
+		combineVCF.annotateVcfFile();
+
+		context.close();
 	}
 
 	/**
@@ -113,24 +102,18 @@ public class CombineVCF
 	 */
 	public void parseCommandLineArgs(String[] args) throws Exception
 	{
-		if (!(args.length == 3))
+		if (!(args.length == 2))
 		{
-			throw new Exception("Must supply 3 arguments");
+			throw new Exception("Must supply 2 arguments");
 		}
 
-		File vcfFile = new File(args[0]);
+		vcfFile = new File(args[0]);
 		if (!vcfFile.isFile())
 		{
 			throw new Exception("Input VCF file does not exist or is not a directory: " + vcfFile.getAbsolutePath());
 		}
 
-		File dataFile = new File(args[1]);
-		if (!dataFile.isFile())
-		{
-			throw new Exception("Data file does not exist or is not a directory: " + dataFile.getAbsolutePath());
-		}
-
-		File outputFile = new File(args[2]);
+		outputFile = new File(args[1]);
 		if (!outputFile.isFile())
 		{
 			throw new Exception("Output file does not exist or is not a directory: " + outputFile.getAbsolutePath());
--- src/main/java/org/molgenis/data/annotation/graduation/utils/AnnotatorUtils.java
+++ src/main/java/org/molgenis/data/annotation/graduation/utils/AnnotatorUtils.java
@@ -12,24 +12,97 @@ import org.molgenis.util.ApplicationContextProvider;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * Class to register and use annotators
+ * 
+ * @author mbijlsma
+ *
+ */
 public class AnnotatorUtils
 {
-	private static final File exacFile = new File("/Users/molgenis/Downloads/ExAC.r0.3.sites.vep.vcf.gz");
+	private static final String THOUSAND_GENOMES_ANNOTATOR_NAME = "thousandGenomes";
+	private static final String GONL_ANNOTATOR_NAME = "gonl";
+	private static final String CADD_ANNOTATOR_NAME = "cadd";
+	private static final String EXAC_ANNOTATOR_NAME = "exac";
 
-	public static Iterator<Entity> annotateWithExac(VcfRepository vcfRepository)
+	private static final File exacFile = new File("/Users/molgenis/Downloads/ExAC.r0.3.sites.vep.vcf.gz");
+	private static final File caddFile = new File("");
+	private static final File gonlFile = new File("");
+	private static final File thousandGenomesFile = new File("");
+
+	private static RepositoryAnnotator registerAnnotator(String annotator)
 	{
 		ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
 		Map<String, RepositoryAnnotator> annotators = applicationContext.getBeansOfType(RepositoryAnnotator.class);
-		RepositoryAnnotator exacAnnotator = annotators.get("exac");
+		RepositoryAnnotator repositoryAnnotator = annotators.get(annotator);
+
+		return repositoryAnnotator;
+	}
+
+	public static Iterator<Entity> annotateWithExac(Iterator<Entity> vcfRepositoryIterator)
+	{
+		RepositoryAnnotator exacAnnotator = registerAnnotator(EXAC_ANNOTATOR_NAME);
+		exacAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(exacFile.getAbsolutePath());
+		return exacAnnotator.annotate(vcfRepositoryIterator);
+	}
+
+	public static Iterator<Entity> annotateWithExac(VcfRepository vcfRepository)
+	{
+		RepositoryAnnotator exacAnnotator = registerAnnotator(EXAC_ANNOTATOR_NAME);
 		exacAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(exacFile.getAbsolutePath());
 		return exacAnnotator.annotate(vcfRepository);
 	}
 
-	public static AnnotationConfigApplicationContext registerCommandLineAnnotator() {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("org.molgenis.data.annotation");
+	public static Iterator<Entity> annotateWithCadd(Iterator<Entity> vcfRepositoryIterator)
+	{
+		RepositoryAnnotator caddAnnotator = registerAnnotator(CADD_ANNOTATOR_NAME);
+		caddAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(caddFile.getAbsolutePath());
+		return caddAnnotator.annotate(vcfRepositoryIterator);
+	}
+
+	public static Iterator<Entity> annotateWithCadd(VcfRepository vcfRepository)
+	{
+		RepositoryAnnotator caddAnnotator = registerAnnotator(CADD_ANNOTATOR_NAME);
+		caddAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(caddFile.getAbsolutePath());
+		return caddAnnotator.annotate(vcfRepository);
+	}
+
+	public static Iterator<Entity> annotateWithGoNl(Iterator<Entity> vcfRepositoryIterator)
+	{
+		RepositoryAnnotator gonlAnnotator = registerAnnotator(GONL_ANNOTATOR_NAME);
+		gonlAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(gonlFile.getAbsolutePath());
+		return gonlAnnotator.annotate(vcfRepositoryIterator);
+	}
+
+	public static Iterator<Entity> annotateWithGoNl(VcfRepository vcfRepository)
+	{
+		RepositoryAnnotator gonlAnnotator = registerAnnotator(GONL_ANNOTATOR_NAME);
+		gonlAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(gonlFile.getAbsolutePath());
+		return gonlAnnotator.annotate(vcfRepository);
+	}
+
+	public static Iterator<Entity> annotateWithThousandGenomes(Iterator<Entity> vcfRepositoryIterator)
+	{
+		RepositoryAnnotator thousandGenomesAnnotator = registerAnnotator(THOUSAND_GENOMES_ANNOTATOR_NAME);
+		thousandGenomesAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(
+				thousandGenomesFile.getAbsolutePath());
+		return thousandGenomesAnnotator.annotate(vcfRepositoryIterator);
+	}
+
+	public static Iterator<Entity> annotateWithThousandGenomes(VcfRepository vcfRepository)
+	{
+		RepositoryAnnotator thousandGenomesAnnotator = registerAnnotator(THOUSAND_GENOMES_ANNOTATOR_NAME);
+		thousandGenomesAnnotator.getCmdLineAnnotatorSettingsConfigurer().addSettings(
+				thousandGenomesFile.getAbsolutePath());
+		return thousandGenomesAnnotator.annotate(vcfRepository);
+	}
+
+	public static AnnotationConfigApplicationContext registerCommandLineAnnotator()
+	{
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				"org.molgenis.data.annotation");
 		context.register(CommandLineAnnotatorConfig.class);
-		
+
 		return context;
 	}
-	
 }
